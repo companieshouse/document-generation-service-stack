cef77e6957b6aaf7ae4fc8bf09baf5c816189d3c
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2023-09-06 14:00:50 +0100

    Use full stack name

--- groups/infrastructure/locals.tf
+++ groups/infrastructure/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  stack_name     = "document-generation"
+  stack_name     = "document-generation-service"
   stack_fullname = "${local.stack_name}-stack"
   name_prefix    = "${local.stack_name}-${var.environment}"
 
